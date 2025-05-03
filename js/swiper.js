165cdb704434574979d3d872bc7936b4e5bdf568
Ostapchuk-Roman <roma.ostapchuk2016@mail.com>
2025-05-03 20:30:21 +0300

    the swiper adaptive is done

--- js/swiper.js
+++ js/swiper.js
@@ -1,13 +1,35 @@
 const swiper = new Swiper(".swiper", {
   slidesPerView: 3,
-  spaceBetween: 0,
-  centeredSlides: true,
+
   loop: true,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    481: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    768: {
+      spaceBetween: 20,
+    },
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 
   navigation: {
     nextEl: ".swiper__button-next",
     prevEl: ".swiper__button-prev",
     clickable: true,
+  },
+
+  pagination: {
+    el: ".swiper__pagination",
+    clickable: true, // дозволяє натискати на буллети
   },
 
   scrollbar: {
